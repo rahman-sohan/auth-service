ff774bbf892c7cc385d332d63a58564153efc5b9
rahman-sohan <Sohanur@Scalebridger.com>
2025-05-31 01:34:40 +0600

    feat: initialize NestJS application with basic structure and configuration

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true });
+  app.setGlobalPrefix('api/v1/auth');
+  
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`''''''''''Auth Service is running on port ${process.env.PORT ?? 3000}'''''''''`);
+}
+bootstrap();
